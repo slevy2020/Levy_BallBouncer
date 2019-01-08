a4a0ea827e5b6f04405144dc60b504e95b08dabb
slevy2020 <slevy@chadwickschool.org>
2019-01-08 12:07:08 -0800

    feat: add content from lesson 5

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Button start = GetComponent<Button>();
+      start.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void StartGame()
+    {
+      //on left mouse click go to level1 Scene
+      Debug.Log("starting game");
+      SceneManager.LoadScene("level1");
+    }
+}
