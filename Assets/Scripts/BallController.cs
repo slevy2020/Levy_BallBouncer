484f99c801826f5a127766bd9d7b86656bad701d
slevy2020 <slevy@chadwickschool.org>
2019-01-13 21:01:40 -0800

    feat: add content from lesson 15
    also created prefabs for the obstacles, should help me later when i'm making new levels

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -13,6 +13,7 @@ public class BallController : MonoBehaviour {
   //private variables
   private int score; //the score this ball has earned
   private Text scoreUI; //pointer to the UI Text Object
+  private PersistentData persistentScript; //pointer to the persistent data
 
     // Start is called before the first frame update
     void Start() {
@@ -22,6 +23,8 @@ public class BallController : MonoBehaviour {
       score = 0;
       //find the scoreUI object
       scoreUI = GameObject.Find("UI Text - Score").GetComponent<Text>();
+      //get control of the persistent data
+      persistentScript = GameObject.Find("PersistentObject").GetComponent<PersistentData>();
     }
 
     // Update is called once per frame
@@ -33,10 +36,15 @@ public class BallController : MonoBehaviour {
       //handle collisions
       if (collision.gameObject.name == "CollisionWallBottom") {
         //hit bottom wall, lose, end the game
+        persistentScript.SetWin(false); //we lost!
         SceneManager.LoadScene("end");
       }
       if (collision.gameObject.name == "GoalTop") {
-        //hit the goal, win, end the game
+        //hit the goal, win, triple score and end the game
+        score *= 3;
+        persistentScript.AddScore(score); //add to the collective score
+        persistentScript.SetWin(true); //We won!
+        persistentScript.LevelComplete(); //another level completed
         SceneManager.LoadScene("end");
       }
       if (collision.gameObject.tag == "Obstacle") {
